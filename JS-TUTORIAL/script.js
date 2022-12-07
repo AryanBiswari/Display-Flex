4a154e77b677bfdefefeaf3615a71e37232550f5
Aryan <aryanbiswari14@gmail.com>
2022-12-07 18:35:08 +0530

    callback completed

--- JS-TUTORIAL/script.js
+++ JS-TUTORIAL/script.js
@@ -1,10 +1,15 @@
-function loadScripts(filename) {
+function loadScripts(filename, callback) {
     let script = document.createElement('script');
     script.src = filename;
     script.onload = function(){
         console.log("Script loaded" + filename);
+        callback(filename);
     }
     document.body.appendChild(script);
 }
 
-loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js");
+function goodNight(name) {
+    alert("Good night!" + name);
+}
+
+loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", goodNight);
