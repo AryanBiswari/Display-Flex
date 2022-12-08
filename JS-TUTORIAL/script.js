fcb882be53338ab647add63752c1ab43b2532aff
Aryan <aryanbiswari14@gmail.com>
2022-12-08 17:54:16 +0530

    promises started

--- JS-TUTORIAL/script.js
+++ JS-TUTORIAL/script.js
@@ -1,48 +1,62 @@
-function loadScripts(filename, callback) {
-    let script = document.createElement('script');
-    script.src = filename;
-    script.onload = function(){
-        console.log("Script loaded" + filename);
-        callback(filename);
-    }
-    document.body.appendChild(script);
-}
+// function loadScripts(filename, callback) {
+//     let script = document.createElement('script');
+//     script.src = filename;
+//     script.onload = function(){
+//         console.log("Script loaded" + filename);
+//         callback(filename);
+//     }
+//     document.body.appendChild(script);
+// }
 
-function goodNight(name) {
-    alert("Good night!" + name);
-}
+// function goodNight(name) {
+//     alert("Good night!" + name);
+// }
 
-function goodEvening(name) {
-    alert("Good evening!" + name);
-}
-loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
-    alert("Good night!" + name);
+// function goodEvening(name) {
+//     alert("Good evening!" + name);
+// }
+// loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
+//     alert("Good night!" + name);
 
-    loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
-        alert("Good night!" + name);
+//     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
+//         alert("Good night!" + name);
     
-    })
-    loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
-        alert("Good night!" + name);
+//     })
+//     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
+//         alert("Good night!" + name);
     
-    })
-    loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
-        alert("Good night!" + name);
+//     })
+//     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
+//         alert("Good night!" + name);
     
-    })
-    loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
-        alert("Good night!" + name);
+//     })
+//     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
+//         alert("Good night!" + name);
     
-    })
-    loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
-        alert("Good night!" + name);
+//     })
+//     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
+//         alert("Good night!" + name);
     
-    })
-    loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
-        alert("Good night!" + name);
+//     })
+//     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
+//         alert("Good night!" + name);
     
-    })
-    loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
-        alert("Good night!" + name);
+//     })
+//     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
+//         alert("Good night!" + name);
     
-    })})
+//     })})
+
+console.log("Hello aryan!");
+setTimeout(function() {
+    console.log("Good night!");
+},3000);
+console.log("Good morning!");
+
+
+let promise = new Promise(function(resolve, reject) {
+    alert("hello raghav!");
+    resolve(45);
+});
+
+console.log(promise);
