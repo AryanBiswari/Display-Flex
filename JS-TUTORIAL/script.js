fc6b56a08da72d92cf92f5e7eefca6ff0833d5e5
Aryan <aryanbiswari14@gmail.com>
2022-12-11 19:57:20 +0530

    try catch finally and async await completed

--- JS-TUTORIAL/script.js
+++ JS-TUTORIAL/script.js
@@ -20,31 +20,31 @@
 
 //     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
 //         alert("Good night!" + name);
-    
+
 //     })
 //     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
 //         alert("Good night!" + name);
-    
+
 //     })
 //     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
 //         alert("Good night!" + name);
-    
+
 //     })
 //     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
 //         alert("Good night!" + name);
-    
+
 //     })
 //     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
 //         alert("Good night!" + name);
-    
+
 //     })
 //     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
 //         alert("Good night!" + name);
-    
+
 //     })
 //     loadScripts("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js", function goodNight(name) {
 //         alert("Good night!" + name);
-    
+
 //     })})
 
 // console.log("Hello aryan!");
@@ -53,14 +53,12 @@
 // },3000);
 // console.log("Good morning!");
 
-
 // let promise = new Promise(function(resolve, reject) {
 //     alert("hello raghav!");
 //     resolve(45);
 // });
 
 // console.log(promise);
-
 
 // let promise = new Promise(function(resolve, reject) {
 //     console.log("Promise is pending...");
@@ -90,8 +88,6 @@
 //     console.log("some error");
 // })
 
-
-
 // const loadScripts = (src) =>{
 //     return new Promise(function(resolve, reject){
 //         let script = document.createElement("script");
@@ -115,25 +111,23 @@
 //     console.log(err);
 // })
 
+// let p1 = new Promise(function(resolve,reject) {
+//     setTimeout(function() {
+//         resolve("promise 1 is resolved!");
+//     },11000);
+// })
 
-let p1 = new Promise(function(resolve,reject) {
-    setTimeout(function() {
-        resolve("promise 1 is resolved!");
-    },11000);
-})
-
-let p2 = new Promise(function(resolve,reject) {
-    setTimeout(function() {
-        reject(new Error());
-        // resolve("promise 2 is resolved!");
-    },2000);
-})
-let p3 = new Promise(function(resolve,reject) {
-    setTimeout(function() {
-        resolve("promise 3 is resolved!");
-    },3000);
-})
-
+// let p2 = new Promise(function(resolve,reject) {
+//     setTimeout(function() {
+//         reject(new Error());
+//         // resolve("promise 2 is resolved!");
+//     },2000);
+// })
+// let p3 = new Promise(function(resolve,reject) {
+//     setTimeout(function() {
+//         resolve("promise 3 is resolved!");
+//     },3000);
+// })
 
 // let promiseAll = Promise.all([p1,p2,p3]);
 // promiseAll.then((value) => {
@@ -147,8 +141,7 @@ let p3 = new Promise(function(resolve,reject) {
 //     console.log(value);
 // }).catch((err) => {
 //     console.log(err);
-// }) 
-
+// })
 
 // let promiseRace = Promise.race([p1,p2,p3]);
 // promiseRace.then((value) => {
@@ -156,7 +149,6 @@ let p3 = new Promise(function(resolve,reject) {
 // }).catch((err) => {
 //     console.log(err);
 // })
-
 
 // let promiseAny = Promise.any([p1,p2,p3]);
 // promiseAny.then((value) => {
@@ -180,3 +172,49 @@ let p3 = new Promise(function(resolve,reject) {
 // }).catch((err) => {
 //     console.log(err);
 // })
+
+// async function calling(){
+//     let t1 = setTimeout(() =>{
+//         console.log("Hii I am Timeout! 1");
+//     }, 0000)
+
+//     let t2 = setTimeout(() =>{
+//         console.log("Hii I am Timeout! 2");
+//     },1000)
+//     return [t1,t2];
+// }
+// calling()
+// console.log(calling()); that is how we check if function returns  a promise or not.
+
+// async function getValue() {
+//   // wait for the value variable to be resolved
+//   const value = await someAsyncOperation();
+
+//   // once the value is resolved, use it in the rest of the function
+//   return doSomethingWith(value);
+// }
+
+// async function main() {
+//   // wait for the getValue function to resolve
+//   const result = await getValue();
+
+//   // use the result of the getValue function
+//   console.log(result);
+// }
+
+
+
+// try{
+//     console.log(hello);
+// }
+// catch(e){
+//     console.error("Error: " + e.message);
+// }
+// finally{
+//     console.log("Hello this program always run !")
+// }
+
+// console.log("Script is running properly")
+
+// try and catch is used to hhandle exceptions  or errors but if we get error in catch block then progem doesnot run 
+// but after finally it always runs
